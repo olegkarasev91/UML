5050ae761ccb2d0e5ab0030090b34276554397bd
Олег <@LAPTOP-A95VS17A>
2022-02-01 20:37:02 +0700

    HomeWork

--- UML.cpp
+++ UML.cpp
@@ -1,14 +1,17 @@
 ﻿// UML.cpp 
 #include <iostream>
+#include<string>
 using namespace std;
 #define MAX_TANK_VOLUME 80
 #define MIN_TANK_VOLUME 40
 #define MIN_ENGINE_CONSUMPTION 4
 #define MAX_ENGINE_CONSUMPTION 25
-class Tank
+
+class Tank // бак
 {
-	const unsigned int VOLUME;
-	double fuel_level;
+protected:
+	const unsigned int VOLUME; // объем бака
+	double fuel_level; // уровень топлива
 public:
 	const unsigned int get_VOLUME()const
 	{
@@ -41,17 +44,18 @@ public:
 	{
 		cout << "Tank is gone\t" << this << endl;
 	}
-	void info()const
+	void info_Tank()const
 	{
 		cout << "Tank volume:\t" << VOLUME << endl;
 		cout << "Fuel level:\t" << fuel_level << endl;
 	}
 };
 
-class Engine
+class Engine:public Tank // двигатель
 {
-	double consumption;
-	double consumption_per_second;
+protected:
+	double consumption;             //расход
+	double consumption_per_second; // расход в секунду
 	bool is_started;
 public:
 	double get_consumption()const
@@ -66,13 +70,20 @@ public:
 	{
 		return is_started;
 	}
-	bool start()
+	bool start(char M)
 	{
-		is_started = true;
+		if (M == 'Y'&&get_fuel_level()!=0)
+			return true;
+		else return false;
 	}
 	bool stop()
 	{
 		is_started = false;
+	}
+	void started(char M)
+	{
+		if (start(M))is_started = true;
+		else is_started = false;
 	}
 	void set_consumption(double consuption)
 	{
@@ -82,7 +93,7 @@ public:
 			this->consumption = MAX_ENGINE_CONSUMPTION / 2;
 		consumption_per_second = consuption * 3e-4;
 	}
-	explicit Engine(double consumption)
+	explicit Engine(unsigned int volume, double consumption):Tank(volume)
 	{
 		set_consumption(consumption);
 		is_started = false;
@@ -92,11 +103,62 @@ public:
 	{
 		cout << "Engine is gone:\t" << this << endl;
 	}
-	void info()const
+	void info_Engine()const
 	{
 		cout << "Consumption:\t" << consumption << endl;
 		cout << "Consumption per sec:\t" << consumption_per_second << endl;
 		cout << "Engine is " << (is_started ? "started" : "stopped") << endl;
+	}
+};
+class Car:public Engine
+{
+	string model;
+	string color;
+	long long int price;
+public:
+	const string& get_model()const
+	{
+		return model;
+	}
+	const string& get_color()const
+	{
+		return color;
+	}
+
+	double get_price()const
+	{
+		return price;
+	}
+	void set_model(const string& model)
+	{
+		this->model = model;
+	}
+	void set_color(const string& color)
+	{
+		this->color = color;
+	}
+	void set_price(double price)
+	{
+		this->price = price;
+	}
+	Car(unsigned int volume, double consumption, const string& model,const string& color, long long int price) : Engine(volume, consumption)
+	{
+		set_model(model);
+		set_color(color);
+		set_price(price);
+		cout << "CarConstructor:\t" << this << endl;
+	}
+	~Car()
+	{
+		cout << "CarDestructor:\t" << this << endl;
+	}
+	void info_Car()const
+	{
+		cout << "Model: " << model << endl;
+		cout << "Color: " << color << endl;
+		cout << "price: " << price << endl;
+		info_Tank();
+		info_Engine();
 	}
 };
 //#define TANK_CHECK
@@ -114,7 +176,15 @@ void main()
 		tank.info();
 	}
 #endif//TANK CHECK
-	Engine engine(9);
-	engine.info();
+	//Engine engine(9);
+	//engine.info();
+	Car A(60, 10, "Toyota", "White", 1500000);
+	A.info_Car();
+	A.fill(20);
+	char M;
+	cout << "Завести автомобиль? (Y/N): "; cin >> M;
+	A.start(M);
+	A.started(M);
+	A.info_Car();
 }
 
